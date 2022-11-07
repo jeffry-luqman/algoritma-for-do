ea69a8ed2a174331a2f201a586d2600ae2c8d0da
Jeffry Luqman <jeffry.luqman@gmail.com>
2022-11-07 17:26:38 +0700

    right align

--- latihan-a08.pas
+++ latihan-a08.pas
@@ -3,12 +3,12 @@ hasilnya:
 0  1  1  1  1
 2  0  2  2  2
 3  6  0  3  3
-4  8  12 0  4
-5  10 15 20 0
+4  8 12  0  4
+5 10 15 20  0
 }
 program latihan;
 var
-	b, k, v: Integer;
+	b, k, v : Integer;
 begin
 	for b := 1 to 5 do 
 	begin
@@ -20,10 +20,12 @@ begin
 			else if k < b then
 				v := b * k;
 
-			if v > 9 then
-				write(v, ' ')
+			if k = 1 then
+				write(v)
+			else if v > 9 then
+				write(' ', v)
 			else
-				write(v, '  ');
+				write('  ', v);
 		end;
 		writeln;
 	end;
